9bc3857b342994dac643ff3eb099adf173bf43ca
caojh <jcao1231@outlook.com>
2022-03-23 08:43:03 +0800

    加入activiti模块

---
+++ allinpay-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/liuzhuandan/service/impl/AllinpayUrgencyServiceImpl.java
@@ -0,0 +1,18 @@
+package org.jeecg.modules.liuzhuandan.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.jeecg.modules.liuzhuandan.service.IAllinpayUrgencyService;
+import org.jeecg.modules.liuzhuandan.entity.AllinpayUrgency;
+import org.jeecg.modules.liuzhuandan.mapper.AllinpayUrgencyMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: 1
+ * @Author: jeecg-boot
+ * @Date:   2022-03-16
+ * @Version: V1.0
+ */
+@Service
+public class AllinpayUrgencyServiceImpl extends ServiceImpl<AllinpayUrgencyMapper, AllinpayUrgency> implements IAllinpayUrgencyService {
+
+}
